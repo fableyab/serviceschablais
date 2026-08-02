d07ec6d087320ba77e3edf1c477f0b68dabdf89f
Powalyze <fabrice.fays@outlook.fr>
2026-08-02 21:56:10 +0200

    Prepare production-ready config

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SITE_CONFIG = {
+  siteUrl: "https://serviceschablais.fr",
+  pocketbaseUrl: "https://votre-instance-pocketbase.example.com",
+  stripePublicKey: "pk_test_votre_cle_publique_stripe",
+  contactPhone: "+33456359240",
+  contactEmail: "contact@serviceschablais.fr"
+};
